50e939f9ff9e9686affe1adc7ac00acf3abacc9e
Shreyas Pant <69047353+ShreyP88@users.noreply.github.com>
2020-09-24 17:19:43 -0400

    Add files via upload

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Shreyas Pant on 9/23/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
